abc4ad71fb67ddae2cfad6ca7a7c144cc88cfb06
Rick <Rick.Reijnders@home.nl>
2019-12-06 12:04:36 +0100

    autoimport and annotation

--- R-project/R/1-load.R
+++ R-project/R/1-load.R
@@ -1,9 +1,16 @@
 #-----------------------------------------------------------------------------------------------------#
-# 									(Install &) Load packages
+#							CHECK IF IN CORRECT FOLDER
+#-----------------------------------------------------------------------------------------------------#
+if (!sum(c("R","input","output")%in% list.files(path = "./")) == 3){stop("Data structure does not contain required elements:'R','input','output'")}
+
+
+#-----------------------------------------------------------------------------------------------------#
+# 							(INSTALL &) LOAD PACKAGES
 #-----------------------------------------------------------------------------------------------------#
 # Insert all packages in requiredpackages
 requiredpackages =
   c(
+  "tools",
   "tidyverse",
   "limma"
   )
@@ -16,27 +23,85 @@ for (i in requiredpackages) {
 	if (!requireNamespace(i, quietly = TRUE))
 		BiocManager::install(i, ask = F, dependencies = c("Depends", "Imports"))
 	require(as.character(i), character.only = TRUE)
-	print(i)
+	#print(i)
 }
 
 
 #-----------------------------------------------------------------------------------------------------#
-# 											Load data
+#							LOAD DATA: AUTOIMPORT TRUE
 #-----------------------------------------------------------------------------------------------------#
-# Some examples to import (saves time searching on the web)
-	# R object:
-	# load("DataWorkspace.RData")
+# AUTOIMPORT
+# So the goal of this function is to load all files which exist in the ./input folder. This way all data is imported automatically. If used correctly importing has not to be defined; only coupling of names to rest of data. The idea is randominput -> autoimport -> link to rest of script. 
 
-	# Txt file:
-	# data = read.delim("DataTabDelim.txt",header = T)
+if (s_AutoImport){
 
-	# xlsx file:
-	# data = as.data.frame(readxl::read_xlsx("DataFromExcel.xlsx",sheet=1))
+	# first check if any files in folder
+	if (length(list.files(path = "./input/")) == 0){stop("No data files in ./R/input folder")}
+	
+	# Move into the input folder
+	setwd( "./input/")
+	
+	# loop trough every file and determine extension; then load and put in varible with identical name
+	for (i in list.files(path = "./")){
+		# Get the extension of file
+		temp_Extension = file_ext(i)
+	
+		#if (tempExtension == "Rdata")
+		#	load(i)
+		
+		# cCheck which one it is
+		if (temp_Extension == "txt")
+			temp_data = read.delim(i, header = T)
+			
+		if (temp_Extension == "xlsx")
+			temp_data = as.data.frame(readxl::read_xlsx(i, sheet=1))  # note: only first sheet is imported
+			
+		if (temp_Extension == "csv")
+			temp_data = read.csv(i,header = T)
+			
+		# Assign the data to the correct variable name
+		assign(x = as.character(substr(i, 1, nchar(i)-4)), value = temp_data)
+	}
+	
+	# Move out the input folder; back to root
+	setwd( "..")
+	
+	# Remove the temp_ files
+	rm(list=c("temp_data", "temp_Extension"))
+	
+	#SET UNIQUE DATA NAME TO UNIVERAL NAME
+	# Because 'data' will be used further; the uniquely extracted names need to be set to correct univeral names.
 
-	# CSV file:
-	# data = read.csv("Data.csv",header = T)
+	# Check if user entered some main data name in SETTINGS
+	if(nchar(s_maindataname) == 0){
+		# Message user it needs to do something (make alert!)
+		message("Select the main data from following options:")
+		
+		# list of names:
+		if(TRUE){print(as.character(substr(list.files(path = "./input/"), 1, nchar(list.files(path = "./input/"))-4)))}
 
-data = read.delim("./input/data-name-1.txt",header = T)
+		# Read the input from user
+		s_maindataname <- readline(prompt = "Main data name: ")
+	}
+
+# Store name in data
+Data = get(s_maindataname)
+#@RRR MORE DATAs
+}
 
 
-@RRR identify de dingen vanzelf list.files(path = "./input/")
+#-----------------------------------------------------------------------------------------------------#
+#							LOAD DATA: AUTOIMPORT FALSE
+#-----------------------------------------------------------------------------------------------------#
+# If autoimport is false; use manual import
+if (!s_AutoImport){
+	warning("make sure this part is correct!")
+	
+	# set the file to correct path
+	Data = read.delim("path/to/folder", header = T)
+}
+
+
+
+
+
