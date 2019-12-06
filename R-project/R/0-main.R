1d221031d7c11e6d67ef9a5c57bf3192b120b8bc
Rick <Rick.Reijnders@home.nl>
2019-12-06 12:04:56 +0100

    annotation and settings autoimport

--- R-project/R/0-main.R
+++ R-project/R/0-main.R
@@ -1,13 +1,14 @@
 #-----------------------------------------------------------------------------------------------------#
 # 							GENERAL INFORMATION
 #-----------------------------------------------------------------------------------------------------#
-# Copyright statement comment:
-#   No Copyright statement, up to Maastricht University to decide.
-
+# Licence information:
+# GNU GENERAL PUBLIC LICENSE 
+# Please see the github folder:
+# https://github.com/Rrtk2/R-project-blank
 
 # Author comment:
-#   Rick Reijnders i6167500
-#   ra.reijnders@student.maastrichtuniversity.nl
+#   Rick Reijnders
+#   rick.reijnders@home.nl
 #   Script version: XX-XX-2019
 
 
@@ -25,22 +26,50 @@
 #		This is created....
 #
 
+# For this workflow to work the structure must be:
+#	/My-project
+#		/input							(data files)
+#			/my-data-set.csv
+#		/R							(code)
+#			/load.R 					(data import)
+#			/clean.R 					(data cleanup)
+#			/fucntion.R 					(defined functions)
+#			/analysis.R 					(pipeline)
+#			/grapics.R 					(post processing)
+#		/output
+#			/session-2019-12-04				(session ID)
+#				/results.txt			(resulted txt files)
+#				/graphics				(resulted images)
+#					/figure1.png
+
+# NOTE: during the creation of this script, assumed was to create a gene expression microarray dataset analysis pipeline.
 
 #-----------------------------------------------------------------------------------------------------#
 # 							SETTINGS
 #-----------------------------------------------------------------------------------------------------#
+# All settings should have an lowercase & underscore prefix (s_Settingvariable1)
 # General settings
 options(stringsAsFactors = F)
 
 # Setting seed to keep consistent results 
 set.seed(123)
 
+# Define root folder (if needed):
+s_rootFolder = "D:/Github/R-project-blank/R-project"
+
+# Use the autoimporter; all files in ./R/input/ will be imported and stored in variable using the name.
+# Works with: .txt .csv .xlsx (can be expanded easally)
+s_AutoImport = TRUE
+
+# s_maindataname can be set to the name of the to be selected main data; skips the selection step when loading data. Leave empty to select later.
+s_maindataname = "data_real"
 
 #-----------------------------------------------------------------------------------------------------#
 #							WORKFLOW
 #-----------------------------------------------------------------------------------------------------#
-setwd("D:\\Github\\R-project-blank\\R-project")
 
+# Set the top root folder (/My-project)
+setwd(s_rootFolder)
 
 # Loads packages and data
 source("./R/1-load.R")
