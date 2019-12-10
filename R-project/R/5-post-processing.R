49ac092137112d960dc99e545073e4209bf1f59a
Rick <Rick.Reijnders@home.nl>
2019-12-10 13:40:31 +0100

    Added basic file saving

--- R-project/R/5-post-processing.R
+++ R-project/R/5-post-processing.R
@@ -1,9 +1,23 @@
 #-----------------------------------------------------------------------------------------------------#
+#							Create Result folder
+#-----------------------------------------------------------------------------------------------------#
+temp_currentdir = "./output/"
+#-----------------------------------------------------------------------------------------------------#
 #							GET OBJECTS WHICH NEED PLOTTED OR STORED
 #-----------------------------------------------------------------------------------------------------#
 # @RRR note to self: define method to do this; plot image is structural thing;always has xlab and ylab; size ect.
 # make dataframe which has this info and make this part univeral!!
 
+# Write the data in in a tab-delim manner, using commas as comma.
+write.table(x = DataResult, file = paste(temp_currentdir, "/", 
+"DataResult.txt", sep = ""), sep = "\t", row.names = T,
+col.names = NA, dec = ".", na = "")  # Dot as comma!
+
+#-----------------------------------------------------------------------------------------------------#
+#							Remove temp_ files/variables
+#-----------------------------------------------------------------------------------------------------#
+# Detect all variables and remove
+rm(list = ls()[grep("temp_",x = ls())])
 
 #-----------------------------------------------------------------------------------------------------#
 #							VERSIONS
@@ -12,3 +26,6 @@
 version
 Sys.info()
 sessionInfo()
+
+
+
